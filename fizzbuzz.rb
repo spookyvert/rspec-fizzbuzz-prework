6716dffecb842a529ec1f3cf0aacf8d163ea56d6
Shemar Dacosta <dshemar6@gmail.com>
2018-11-24 23:47:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
+    return "FizzBuzz"
   elsif int % 5 == 0
-    puts "Buzz"
+    return "Buzz"
   elsif int % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   end
 end
